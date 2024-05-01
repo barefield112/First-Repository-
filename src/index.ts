22a651cf56cd2369c3c6edc6d32a9c2c8d8614aa
barefield112 <gagebarefield12@gmail.com>
2024-05-01 19:18:39 -0400

    Says Hi to Gage

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
+  const name = 'Gage Barefield';
   greetUser(name);
+  /* let num = 0;
+  while (num <= 100) {
+    console.log(num);
+    num = num + 1;
+  }*/
 });
